4981c5d7a39a399e1a5473b1923f0132a1ab05a0
Roy Mazumder <rm2033@princeton.edu>
2022-12-07 20:03:13 -0500

    fewf

---
+++ mywc.s
@@ -0,0 +1,11 @@
+//----------------------------------------------------------------------
+// mywc.s
+// Author: Roy Mazumder
+//----------------------------------------------------------------------
+
+    .section .data
+
+lLineCount: .quad 0
+lWordCount: .quad 0
+lCharCount: .quad 0
+iInWord: .word 0
