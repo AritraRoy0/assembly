d07a8a8537165a0ba19b1124699d8f9dfb1d013b
Roy Mazumder <rm2033@princeton.edu>
2022-12-09 16:32:35 -0500

    fewf

--- generator.c
+++ generator.c
@@ -5,7 +5,7 @@ int main(void) {
 
     int chr;
 
-    for (int i = 0, i < 4800, i++){
+    for (int i = 0; i < 4800; i++){
         chr = rand() % 0x7F;
 
         if (chr == 0x09 || chr == 0x0A || (chr <= 0x7E && chr >= 0x20)){
